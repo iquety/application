4f778ddb3be549672fb06594fbbd8d4f9911280b
Ricardo Pereira <contato@ricardopedias.com.br>
2024-09-25 18:24:55 -0300

    Aumento na cobertura de testes para os descritores

--- tests/ConsoleEngine/ConsoleDescriptorTest.php
+++ tests/ConsoleEngine/ConsoleDescriptorTest.php
@@ -4,17 +4,39 @@ declare(strict_types=1);
 
 namespace Tests\Run;
 
-use Iquety\Application\IoEngine\Console\ConsoleEngine;
-use Iquety\Application\IoEngine\ModuleSet;
-use Iquety\Injection\Container;
+use Iquety\Application\IoEngine\Console\ConsoleDescriptor;
+use Iquety\Application\IoEngine\Console\ConsoleModule;
+use Iquety\Application\IoEngine\Console\Script;
+use Tests\IoEngine\Stubs\FrontController\GetCommand;
 use Tests\TestCase;
 
 class ConsoleDescriptorTest extends TestCase
 {
     /** @test */
-    public function unknown(): void
+    public function getters(): void
     {
-        $this->markTestIncomplete();
-        
+        $descriptor = new ConsoleDescriptor(
+            Script::class,
+            ConsoleModule::class,
+            '',
+            ''
+        );
+
+        $this->assertSame(Script::class, $descriptor->type());
+        $this->assertSame(ConsoleModule::class, $descriptor->module());
+        $this->assertSame('::', $descriptor->action());
+    }
+
+    /** @test */
+    public function factory(): void
+    {
+        $descriptor = ConsoleDescriptor::factory(ConsoleModule::class, 'saida de terminal', -1);
+
+        $this->assertSame(Script::class, $descriptor->type());
+        $this->assertSame(ConsoleModule::class, $descriptor->module());
+        $this->assertSame('::', $descriptor->action());
+
+        $this->assertSame('saida de terminal', $descriptor->output());
+        $this->assertSame(-1, $descriptor->status());
     }
 }
